c91e6055e5d9a6c6715327a603d67c9a85ef75d4
scdevlin90 <60587106+scdevlin90@users.noreply.github.com>
2023-09-14 18:36:02 -0400

    wine reviews assignment

--- reviews.py
+++ reviews.py
@@ -1,2 +1,12 @@
-# add your code here
+import pandas as pd
+reviews = pd.read_csv("data/winemag-data-130k-v2.csv.zip", index_col=0)
 
+countries = reviews.country.unique()
+reviews_per_country = reviews.country.value_counts()
+median_points = reviews.points.median()
+
+reviews.apply(row.country, axis = 'columns')
+
+wine_reviews = pd.DataFrame({countries, reviews_per_country, median_points})
+
+csv_file = wine_reviews.to_csv('reviews-per-country.csv')
